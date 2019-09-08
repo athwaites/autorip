818aff5d77d375f6b7b457352445e048986eb223
Adam Thwaites <thwaites.adam@gmail.com>
2019-09-08 20:48:52 +1000

    Range of fixes to get transcode working

--- ripbd.sh
+++ ripbd.sh
@@ -8,11 +8,15 @@ get_config_var() {
     echo "$(awk -F '=' "/^$1/{print \$2}" $CONFIG_PATH)"
 }
 VIDEO_WORKING_PATH=$(get_config_var VIDEO_WORKING_PATH)
+# Protection to make sure the working path is NOT root directory
+if [ "$VIDEO_WORKING_PATH" == "/" ] ; then
+    exit 1
+fi
 VIDEO_WORKING_PATH=${VIDEO_WORKING_PATH%/}
-WORKING_PATH="$VIDEO_WORKING_PATH/${ID_FS_LABEL:0:8}"
+WORKING_PATH="$VIDEO_WORKING_PATH"/"${ID_FS_LABEL:0:8}"
 WORKING_NAME="disc"
 VIDEO_RIPPER_BIN=$(get_config_var VIDEO_RIPPER_BIN)
-VIDEO_REJECT_RATIO=$(get_config_var VIDEO_RIPPER_BIN)
+VIDEO_REJECT_FACTOR=$(get_config_var VIDEO_REJECT_FACTOR)
 TRANSCODER_BIN=$(get_config_var TRANSCODER_BIN)
 TRANSCODER_CONTAINER_FORMAT=$(get_config_var TRANSCODER_CONTAINER_FORMAT)
 TRANSCODER_VIDEO_FORMAT=$(get_config_var TRANSCODER_VIDEO_FORMAT)
@@ -25,16 +29,16 @@ touch_dir() {
     fi
 }
 
-# Ensure the required media directories are available
-touch_dir "$VIDEO_WORKING_PATH"
-touch_dir "$WORKING_PATH"
-
 # Get the disc working path
-find /disc.rip/ -maxdepth 1 -type d -name "test*"
 LAST_DISC_PATH=$(find "$WORKING_PATH" -maxdepth 1 -type d -name "$WORKING_NAME*" | tail -1)
 LAST_DISC_NUM=$(grep -Eo '[0-9]+$' <<< $LAST_DISC_PATH)
 DISC_NAME=$(printf '%s_%02d' "$WORKING_NAME" $(($LAST_DISC_NUM + 1)))
 DISC_WORKING_PATH="$WORKING_PATH/$DISC_NAME"
+
+# Ensure the required media directories are available
+touch_dir "$VIDEO_WORKING_PATH"
+touch_dir "$WORKING_PATH"
+touch_dir "$DISC_WORKING_PATH"
 
 # Execute rip
 # $VIDEO_RIPPER_BIN mkv dev:$DEVNAME all $DISC_WORKING_PATH -r
@@ -42,19 +46,23 @@ DISC_WORKING_PATH="$WORKING_PATH/$DISC_NAME"
 # Find largest file size in directory and reject everything below the size
 # factor limit (deletes random titles, special features, etc; leaves just
 # the movie or episodes).
-LARGEST_FILE=$(ls -Sa "$DISC_WORKING_PATH/*.mkv" | head -1)
+LARGEST_FILE=$(ls -Sa "$DISC_WORKING_PATH"/*.mkv | head -1)
 LARGEST_FILE_SIZE=$(wc -c < "$LARGEST_FILE")
 find "$DISC_WORKING_PATH" -maxdepth 1 -name "*.mkv" -size -"$(($LARGEST_FILE_SIZE / $VIDEO_REJECT_FACTOR))"c -delete
 
 # Get next file number in WORKING_PATH
-LAST_FILE=$(ls "$WORKING_PATH"/*.$TRANSCODER_CONTAINER_FORMAT | tail -1)
-LAST_NAME=${LAST_FILE:0:-4}
-LAST_NUM=$(grep -Eo '[0-9]+$' <<< $LAST_NAME)
-CUR_NUM=$(($LAST_NUM + 1))
+LAST_FILE=$(find "$WORKING_PATH" -maxdepth 1 -type f -name "*.$TRANSCODER_CONTAINER_FORMAT" | tail -1)
+if [ -f "$LAST_FILE" ] ; then
+    LAST_NAME=${LAST_FILE:0:-4}
+    LAST_NUM=$(grep -Eo '[0-9]+$' <<< $LAST_NAME)
+    CUR_NUM=$(($LAST_NUM + 1))
+else
+    CUR_NUM=1
+fi
 
 # Convert each of the MKV files pulled from the disc
 for CUR_IN_PATH in "$DISC_WORKING_PATH"/* ; do
-    if [ -d "$CUR_IN_PATH" ] || [ ! ${CUR_IN_PATH:-4} == ".mkv" ] ; then
+    if [ -d "$CUR_IN_PATH" ] || [ ${CUR_IN_PATH: -4} != ".mkv" ] ; then
         # Skip over directories and non-MKV files
         continue
     fi
